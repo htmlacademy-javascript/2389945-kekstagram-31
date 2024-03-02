816781dd273a46e863cdec3e29b9b7f1ad0d5480
hondavod70 <hondavod70@gmail.com>
2024-03-02 20:30:05 +0700

    8.15. Открывается и закрывается (часть 2)

---
+++ js/generate-picture.js
@@ -0,0 +1,87 @@
+import { isEnterKey, isEscapeKey } from './utils';
+import { SHOWN_COMMENTS_COUNT } from './config';
+
+const body = document.querySelector('body');
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureImg = bigPicture.querySelector('img');
+const bigPictureCaption = bigPicture.querySelector('.social__caption');
+const bigPictureLikesCount = bigPicture.querySelector('.likes-count');
+const bigPictureCommentsLoader = bigPicture.querySelector('.comments-loader');
+const bigPictureTotalCommentsCount = bigPicture.querySelector(
+  '.social__comment-total-count'
+);
+const bigPictureShownCommentsCount = bigPicture.querySelector(
+  '.social__comment-shown-count'
+);
+const actBigPictureCancel = bigPicture.querySelector('#picture-cancel');
+const bigPictureComments = bigPicture.querySelector('.social__comments');
+const bigPictureComment = bigPicture.querySelector('.social__comment');
+
+// Закрытие формы полноразмерного просмотра фото
+const closePicture = () => {
+  body.classList.remove('modal-open');
+  bigPicture.classList.add('hidden');
+};
+
+// Действия при нажатии клавиши Escape
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    document.removeEventListener('keydown', onDocumentKeydown);
+    closePicture();
+  }
+};
+
+// Добавление блока с комментариями
+const addComments = (post, commentsCount) => {
+  while (bigPictureComments.firstChild) {
+    bigPictureComments.removeChild(bigPictureComments.firstChild);
+  }
+  post.comments.forEach((item, index) => {
+    const newComment = bigPictureComment.cloneNode(true);
+    newComment.querySelector('img').src = item.avatar;
+    newComment.querySelector('img').alt = item.name;
+    newComment.querySelector('.social__text').textContent = item.message;
+    if (index < commentsCount) {
+      bigPictureComments.appendChild(newComment);
+    }
+  });
+  bigPictureShownCommentsCount.textContent =
+    commentsCount < post.comments.length ? commentsCount : post.comments.length;
+  return SHOWN_COMMENTS_COUNT;
+};
+
+// Открытие формы полноразмерного просмотра фото
+const openPicture = (post) => {
+  let shownComments = SHOWN_COMMENTS_COUNT;
+  bigPictureImg.src = post.url;
+  bigPictureCaption.textContent = post.description;
+  bigPictureLikesCount.textContent = post.likes;
+  bigPictureTotalCommentsCount.textContent = post.comments.length;
+  body.classList.add('modal-open');
+  bigPicture.classList.remove('hidden');
+  while (bigPictureComments.firstChild) {
+    bigPictureComments.removeChild(bigPictureComments.firstChild);
+  }
+  shownComments = shownComments + addComments(post, shownComments);
+  document.addEventListener('keydown', onDocumentKeydown);
+  bigPictureCommentsLoader.addEventListener('click', () => {
+    shownComments = shownComments + addComments(post, shownComments);
+  });
+};
+
+// Событие закрытия формы по клику мыши
+actBigPictureCancel.addEventListener('click', () => {
+  document.removeEventListener('keydown', onDocumentKeydown);
+  closePicture();
+});
+
+// Событие закрытия формы по нажатию Enter на кнопке закрытия
+actBigPictureCancel.addEventListener('keydown', (evt) => {
+  if (isEnterKey(evt)) {
+    document.removeEventListener('keydown', onDocumentKeydown);
+    closePicture();
+  }
+});
+
+export { openPicture };
