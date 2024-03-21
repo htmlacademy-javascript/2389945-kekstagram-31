1c25b2a9e5488f1abae8162036a2e07203736112
hondavod70 <hondavod70@gmail.com>
2024-03-22 00:40:15 +0700

    Наработки

--- js/picture-thumbnails.js
+++ js/picture-thumbnails.js
@@ -1,6 +1,11 @@
-import { fragment, pictureTemplate, pictures, pictureFilters } from './dom-elements.js';
+import {
+  fragment,
+  pictureTemplate,
+  pictures,
+  pictureFilters,
+} from './dom-elements.js';
 import { openPicture } from './picture-comments.js';
-import { pictureState } from './picture-state.js';
+import { pictureState, getPictureById } from './picture-state.js';
 
 // Обработка действия при клике на миниатюре фотографии
 const onPicturesClick = (evt) => {
@@ -13,9 +18,44 @@ const onPicturesClick = (evt) => {
   }
 };
 
+const compareComments = (pictureA, pictureB) => {
+  const rankA = getPictureById(pictureA.id).comments.length;
+  const rankB = getPictureById(pictureB.id).comments.length;
+  return rankB - rankA;
+};
+
 // Создание миниатюр фотографий
-const createPictureThumbnails = () => {
-  pictureState.pictures.forEach((picture) => {
+const createPictureThumbnails = (evt) => {
+  let filteredPictures;
+  //console.log(evt.target);
+  if (evt) {
+    //console.log(evt.target);
+    if (evt.target.id === 'filter-default') {
+      filteredPictures = pictureState.pictures.slice();
+      console.log(filteredPictures);
+    } else if (evt.target.id === 'filter-discussed') {
+      //'filter-random': (pictures) =>
+      //  getUniqueRandomArrayElement(pictures, PICTURE_RANDOM_COUNT),
+
+      filteredPictures = pictureState.pictures.slice().sort(compareComments);
+      console.log(filteredPictures);
+    }
+  } else {
+    filteredPictures = pictureState.pictures.slice();
+  }
+  /*
+  if (evt.target.id === 'filter-default') {
+    filteredPictures = pictureState.pictures.slice();
+  } else if (evt.target.id === 'filter-discussed') {
+    //'filter-random': (pictures) =>
+    //  getUniqueRandomArrayElement(pictures, PICTURE_RANDOM_COUNT),
+    filteredPictures = pictureState.pictures.slice().sort(compareComments);
+  } else {
+    filteredPictures = pictureState.pictures.slice();
+  }
+  */
+
+  filteredPictures.forEach((picture) => {
     const newPicture = pictureTemplate.cloneNode(true);
     const pictureImg = newPicture.querySelector('.picture__img');
     pictureImg.id = picture.id;
@@ -30,5 +70,16 @@ const createPictureThumbnails = () => {
   pictures.appendChild(fragment);
   pictures.addEventListener('click', onPicturesClick);
 };
+
+const onFilterClick = (evt) => {
+  evt.preventDefault();
+  console.log(evt.target);
+  pictures
+    .querySelectorAll('.picture')
+    .forEach((element) => element.parentNode.removeChild(element));
+  createPictureThumbnails(evt);
+};
+
+pictureFilters.addEventListener('click', onFilterClick);
 
 export { createPictureThumbnails };
